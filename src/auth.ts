bc2d4482b759417b101b51ba81e332b44419dfa7
practicanteit02-maker <practicante.it02@omgwgroup.com>
2026-09-04 15:55:25 -0500

    Agregar configuración de auth

---
+++ src/auth.ts
@@ -0,0 +1,12 @@
+import NextAuth from "next-auth";
+import Cognito from "next-auth/providers/cognito";
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  providers: [
+    Cognito({
+      clientId: process.env.AUTH_COGNITO_ID,
+      clientSecret: process.env.AUTH_COGNITO_SECRET,
+      issuer: process.env.AUTH_COGNITO_ISSUER,
+    }),
+  ],
+});
