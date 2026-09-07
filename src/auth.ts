9947d312f9e18d52ebd4a0f45720edde1cc46638
practicanteit02-maker <practicante.it02@omgwgroup.com>
2026-09-07 08:53:45 -0500

    Corregir tipo de idToken en auth config

--- src/auth.ts
+++ src/auth.ts
@@ -3,19 +3,21 @@ import Cognito from "next-auth/providers/cognito";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [
-    Cognito({
-      clientId: process.env.AUTH_COGNITO_ID,
-      clientSecret: process.env.AUTH_COGNITO_SECRET,
-      issuer: process.env.AUTH_COGNITO_ISSUER,
-      checks: ["state"],
+    {
+      ...Cognito({
+        clientId: process.env.AUTH_COGNITO_ID,
+        clientSecret: process.env.AUTH_COGNITO_SECRET,
+        issuer: process.env.AUTH_COGNITO_ISSUER,
+        checks: ["state"],
+        profile(profile) {
+          return {
+            id: profile.sub,
+            name: profile.name ?? profile.email,
+            email: profile.email,
+          };
+        },
+      }),
       idToken: false,
-      profile(profile) {
-        return {
-          id: profile.sub,
-          name: profile.name ?? profile.email,
-          email: profile.email,
-        };
-      },
-    }),
+    },
   ],
 });
